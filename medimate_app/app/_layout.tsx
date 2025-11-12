ec15000a6314fd3d7460eb6103035359fbeaec9f
David Beno <dawid.beno@gmail.com>
2025-11-12 19:07:56 +0100

    bottom tab navigation - icons

--- medimate_app/app/_layout.tsx
+++ medimate_app/app/_layout.tsx
@@ -1,10 +1,22 @@
 import { Tabs } from 'expo-router';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import AntDesign from '@expo/vector-icons/AntDesign';
 
 export default function TabLayout() {
   return (
     <Tabs>
-      <Tabs.Screen name="index" />
-      <Tabs.Screen name="second" />
+      <Tabs.Screen name="index" options={() => ({
+        tabBarShowLabel: false,
+        tabBarIcon: ({ size, color }) => (
+            <Ionicons name="notifications-circle-outline" size={size} color={color} />
+          )
+      })}/>
+      <Tabs.Screen name="second" options={() => ({
+        tabBarShowLabel: false,
+        tabBarIcon: ({ size, color }) => (
+          <AntDesign name="user-add" size={size} color={color} />
+        )
+      })}/>
     </Tabs>
   );
 }
